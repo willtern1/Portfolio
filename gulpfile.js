26d1dbef20ca6a65299190446c1ba102ecc960dc
Alexander Zinchenko <zinchenko.al1aksandr@yandex.ru>
2022-11-28 23:24:39 +0300

    add:files, first section

--- gulpfile.js
+++ gulpfile.js
@@ -32,9 +32,9 @@ gulp.task("watch", function () {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
     gulp.watch("src/*.html").on("change", gulp.parallel("html"));
     gulp.watch("src/js/**/*.js").on("change", gulp.parallel("scripts"));
-    gulp.watch("src/fonts/**/*").on("all", gulp.parallel("fonts"));
-    gulp.watch("src/icons/**/*").on("all", gulp.parallel("icons"));
-    gulp.watch("src/img/**/*").on("all", gulp.parallel("images"));
+    gulp.watch("src/fonts/**/*").on("add", gulp.parallel("fonts"));
+    gulp.watch("src/icons/**/*").on("add", gulp.parallel("icons"));
+    gulp.watch("src/img/**/*").on("add", gulp.parallel("images"));
 });
 
 gulp.task("html", function () {
